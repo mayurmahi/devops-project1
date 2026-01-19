58d38e158eaf9ac6ca56939790d3d9a1f177b10c
mayurmahi <mayurmahindrakar444@gmail.com>
2026-01-19 14:06:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Full DevOps Pipeline");
+});
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
