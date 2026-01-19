6a525140501ae0e7fe9a3f02a2af3808b94cf498
mayurmahi <mayurmahindrakar444@gmail.com>
2026-01-19 18:39:14 +0530

    avc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from mayur i am devops engineer Pipeline  devops piplines");
+  res.send("Hello from mayur  Pipeline  devops piplines");
 });
 
 app.listen(8000, () => {
