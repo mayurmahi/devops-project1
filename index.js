2ed1fffde3d5edff1ca0342040a8a9e585784959
mayurmahi <mayurmahindrakar444@gmail.com>
2026-01-22 09:23:00 +0530

    new comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("this is first devops project made by mayur  mahindrakar");
+  res.send("this is first devops project made by me");
 });
 
 app.listen(8000, () => {
